d91a925d14147f0230758ad808a369ccbfab5e6b
MATHEROS <israilser516@gmail.com>
2022-06-14 20:26:12 +0530

    This bot is is src of pogy but on djs v13

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+ "verification": "",
+ "description": "Pogy is a multi-purpose discord bot ready to skill up and boost up your Discord server!",
+ "domain": "", // domain
+ "google_analitics": "", // google analitics
+ "token": "",
+ "https":"https://", // leave as is
+ "port":"5003",
+
+ "client_id":"939486100114796544", // bot client ID
+ "secret":"IRYKRLfReHdAX5aAuSCrtu8kYrPHEwOy" // bot client secret for auth
+
+}
+
+
+// for dashboard, read more on https://github.com/IgorKowalczyk/majobot
---
+++ shard.js
@@ -0,0 +1,20 @@
+const config = require('./config.json');
+const Discord = require('discord.js');
+const Statcord = require("statcord.js");
+const { token } = require('./utils/variables.js');
+
+
+const manager = new Discord.ShardingManager('./index.js', {
+  token: token,
+ //autoSpawn: true,
+  //totalShards: 'auto'
+  totalShards: 1
+});
+
+manager.spawn();
+
+manager.fetchClientValues('guilds.cache.size')
+  .then(results => console.log(`${results.reduce((prev, val) => prev + val, 0)} total guilds`))
+  .catch(console.error);
+
+manager.on('shardCreate', shard => logger.info(`Launching Shard ${shard.id + 1}`, { label: `Shard` }));
